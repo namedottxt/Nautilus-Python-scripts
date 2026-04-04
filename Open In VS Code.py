a731b44e091d6605bda325e3f5d8c24d8a2a0d07
name.txt <201750255+namedottxt@users.noreply.github.com>
2026-04-04 22:02:50 +0200

    Create Open In VS Code.py

---
+++ Open In VS Code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys, subprocess, os
+
+def main():
+    SELECTION = sys.argv[1:][0]
+    PATH = f'{os.getcwd()}/{SELECTION}'
+
+    subprocess.run(['code', PATH])
+        
+if __name__ == "__main__":
+    main()
